5044d1ac45af2a6bdf16a8e28b865e0e44b02916
Raimundin <32044058+Reymai@users.noreply.github.com>
2019-10-07 11:42:50 +0300

    Design

---
+++ app/src/main/java/com/example/javalearner/LibraryThemeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.javalearner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LibraryThemeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library_theme_list);
+    }
+}
