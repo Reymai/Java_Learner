ee29e69bb434de050de8de6ad3ffcb3bdc6f81ca
NightmareAHouse <nightmareahouse@gmail.com>
2019-12-19 21:58:37 +0200

    add library

--- app/src/main/java/com/example/javalearner/LibraryThemeInfoActivity.java
+++ app/src/main/java/com/example/javalearner/LibraryThemeInfoActivity.java
@@ -2,14 +2,35 @@ package com.example.javalearner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LibraryThemeInfoActivity extends AppCompatActivity {
+
+    TextView mThemeInfoTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library_theme_info);
 
+        int theme = getIntent().getIntExtra("Lib_Theme", 0);
+        mThemeInfoTxt = findViewById(R.id.ThemeInfoTxt);
+        String[] names = new String[0];
+        if (theme == 1) {
+
+        }
+
+        if(theme == 2){
+            names = new String[]{""
+
+            };
+        }
+            mThemeInfoTxt.setText("@strings/Level");
+            for (String name : names) {
+                mThemeInfoTxt.append(name + "\n\n\n\n\n");
+            }
+            
     }
 }
